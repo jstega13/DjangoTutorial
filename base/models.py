a5efe99d8a4cdeb973b5dd069da5650877f55b4a
jstega13 <72770267+Jcstegall@users.noreply.github.com>
2025-02-10 12:28:01 -0500

    .

--- base/models.py
+++ base/models.py
@@ -19,7 +19,9 @@ class Room(models.Model):
     updated =models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
-
+    class Meta:
+        ordering =['-updated', '-created'] #'-' makes it reverse order(newest at the top), no dash means it is decending order
+ 
     def __str__(self):
         return self.name
 
