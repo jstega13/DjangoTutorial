6f7751e4045ddc6288724c76228f256049c7083f
jstega13 <72770267+Jcstegall@users.noreply.github.com>
2025-02-13 20:04:54 -0500

    3:51:00 error

--- base/models.py
+++ base/models.py
@@ -32,6 +32,10 @@ class Message(models.Model):
         body =models.TextField()
         updated =models.DateTimeField(auto_now=True)
         created = models.DateTimeField(auto_now_add=True)
+        
+        class Meta:
+            ordering =['-updated', '-created'] #'-' makes it reverse order(newest at the top), no dash means it is decending order
+ 
 
         def __str__(self):
             return self.body[0:50]
